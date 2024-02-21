a884f0b70343c84f42a3d98a57ef67624e9fc1e8
xxcouzxme <59864473+xxcouzxme@users.noreply.github.com>
2024-02-21 22:25:50 +0300

    Update vpn_script.py

--- vpn_script.py
+++ vpn_script.py
@@ -17,9 +17,9 @@ print(rand)
 #run docker command
 env =('--env \"VPN_USER=' + str(dict['username'])+ '\"'+ ' --env \"VPN_PASSWORD=' + str(dict['password'])+ '\"' + ' --env \"VPN_IPSEC_PSK=' + str(rand) + '\"') 
 print(env)
-print('''docker run --name ipsec-vpn-server \
+#macos l2tp vpn
+os.system('''docker run --name ipsec-vpn-server \
         '''+ env +''' \
-        --env-file ~/workspace/docker/vpn/env \
         --restart=always \
         -p 500:500/udp \
         -p 4500:4500/udp \
